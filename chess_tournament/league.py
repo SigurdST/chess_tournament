4ec13e980139b73e7697a53220d0c2e4f2039519
Sigurd <81868108+SigurdST@users.noreply.github.com>
2024-10-23 16:40:47 +0200

    projet terminé

--- chess_tournament/league.py
+++ chess_tournament/league.py
@@ -1,45 +1,65 @@
 import random
 
 class Tournament:
+
     def __init__(self, teams):
         self.teams = teams
-        self.results = {teams.country: 0 for team in teams}
+        self.results = {team.country: 0 for team in teams}
 
     def simulate_game(self, player1, player2):
+
+        print(f"\n----- Partie {player1.name} vs {player2.name} -----\n")
         # Calcul des probabilités en fonction des classements Elo
-        ratio = player1.elo / player2.elo
-        if - 0.49 < (ratio - 1)  < 0.49:
-            proba_blanc_victoire = 0.5  * (ratio - 1)
-            proba_blanc_nul = 0.5 - (ratio - 1)
-            proba_blanc_defaite = 1 - proba_blanc_victoire - proba_blanc_nul
-        elif (ratio - 1)  >= 0.49:
-            proba_blanc_victoire = 0.98
-            proba_blanc_nul = 0.01
-            proba_blanc_defaite = 1 - proba_blanc_victoire - proba_blanc_nul
+        ratio1 = player1.elo / player2.elo
+        if - 0.99 < (ratio1 - 1)  < 0.99:
+            proba_blanc_victoire1 = 0.5  + (ratio1 - 1)
+            proba_blanc_nul1 = 0.5 - (ratio1 - 1)
+            proba_blanc_defaite1 = 1 - proba_blanc_victoire1 - proba_blanc_nul1
+        elif (ratio1 - 1)  >= 0.99:
+            proba_blanc_victoire1 = 0.98
+            proba_blanc_nul1 = 0.01
+            proba_blanc_defaite1 = 1 - proba_blanc_victoire1 - proba_blanc_nul1
         else:
-            proba_blanc_victoire = 0.01
-            proba_blanc_nul = 0.01
-            proba_blanc_defaite = 1 - proba_blanc_victoire - proba_blanc_nul
+            proba_blanc_victoire1 = 0.01
+            proba_blanc_nul1 = 0.01
+            proba_blanc_defaite1 = 1 - proba_blanc_victoire1 - proba_blanc_nul1
+
+        ratio2 = player2.elo / player1.elo
+        if - 0.99 < (ratio2 - 1) < 0.99:
+            proba_blanc_victoire2 = 0.5 + (ratio2 - 1)
+            proba_blanc_nul2 = 0.5 - (ratio2 - 1)
+            proba_blanc_defaite2 = 1 - proba_blanc_victoire2 - proba_blanc_nul2
+        elif (ratio2 - 1) >= 0.99:
+            proba_blanc_victoire2 = 0.98
+            proba_blanc_nul2 = 0.01
+            proba_blanc_defaite2 = 1 - proba_blanc_victoire2 - proba_blanc_nul2
+        else:
+            proba_blanc_victoire2 = 0.01
+            proba_blanc_nul2 = 0.01
+            proba_blanc_defaite2 = 1 - proba_blanc_victoire2 - proba_blanc_nul2
+
+        
+
 
         score1 = 0
         score2 = 0
 
         # Partie 1: joueur1 a les blancs
-        result = random.choices([1, 0.5, 0], [proba_blanc_victoire, proba_blanc_nul, proba_blanc_defaite])[0]
+        result = random.choices([1, 0.5, 0], [proba_blanc_victoire1, proba_blanc_nul1, proba_blanc_defaite1])[0]
         score1 += result
         score2 += 1 - result
-        print(f"Partie 1 : {player1.name} avec les blancs vs {player2.name} avec les noirs: {result}")
+        print(f"Partie 1 : {player1.name} avec les blancs vs {player2.name} avec les noirs: {result} - {1 - result}\n")
 
         # Partie 2: joueur2 a les blancs (inverser les probabilités)
-        result = random.choices([1, 0.5, 0], [proba_blanc_defaite, proba_blanc_nul, proba_blanc_victoire])[0]
+        result = random.choices([1, 0.5, 0], [proba_blanc_victoire2, proba_blanc_nul2, proba_blanc_defaite2])[0]
         score2 += result
         score1 += 1 - result
-        print(f"Partie 2 : {player2.name} avec les blancs vs {player1.name} avec les noirs: {result}")
+        print(f"Partie 2 : {player2.name} avec les blancs vs {player1.name} avec les noirs: {result} - {1 - result}\n")
 
         return score1, score2
 
     def simulate_duel(self, team1, team2):
-        """Simule une rencontre entre deux équipes. Les joueurs s'affrontent par niveau."""
+        print(f"\n\n--------------- Match {team1.country} vs {team2.country} ---------------\n")
         team1.order_elo()
         team2.order_elo()
         score_equipe1 = 0
@@ -47,8 +67,8 @@ class Tournament:
 
         # Les meilleurs joueurs s'affrontent, puis les deuxièmes meilleurs, etc.
         for i in range(4):
-            joueur1 = team1.players[i]
-            joueur2 = team2.players[i]
+            joueur1 = team1.list_of_players[i]
+            joueur2 = team2.list_of_players[i]
             score1, score2 = self.simulate_game(joueur1, joueur2)
             score_equipe1 += score1
             score_equipe2 += score2
@@ -56,17 +76,19 @@ class Tournament:
 
         self.results[team1.country] += score1
         self.results[team2.country] += score2
+
+        print(f"Résultat final: {team1.country} {score1} - {score2} {team2.country}\n")
    
 
     def simulate_tournament(self):
-
+        print("\n---------- BIENVENUE DANS LA SIMULATION DU TOURNOI D'ECHECS ----------\n")
         for i in range(len(self.teams)):
             for j in range(i + 1, len(self.teams)):
-                self.simulate_duel(self.equipes[i], self.teams[j])
+                self.simulate_duel(self.teams[i], self.teams[j])
 
     def ranking(self):
 
-        classement = sorted(self.res.items(), key=lambda x: x[1], reverse=True)
-        print("CTournament ranking :")
+        classement = sorted(self.results.items(), key=lambda x: x[1], reverse=True)
+        print("Chess ournament ranking :")
         for rang, (country, points) in enumerate(classement, 1):
             print(f"{rang}. {country} - {points} points")
--- chess_tournament/main.py
+++ chess_tournament/main.py
@@ -1,3 +1,50 @@
-import team
-import player
-import league
+from team import Country
+from player import Player
+from league import Tournament
+
+import pandas as pd
+
+
+
+
+
+
+# Function to run the simulation
+def run_simulation():
+
+    chess_players = pd.read_excel('chess_players.xlsx')
+    # Create the list of players
+
+    players = []
+
+    for index, row in chess_players.iterrows():
+        name = row['Player']
+        country = row['Country']
+        elo = row['Elo']
+        player = Player(name, elo, country)
+        players.append(player)
+    
+    # Create the teams
+
+    countries = chess_players['Country'].unique()
+    teams = [Country(country) for country in countries]
+    for player in players:
+        for team in teams:
+            if player.country == team.country:
+                team.add_player(player)
+    
+    # Create the tournament
+
+    tournament = Tournament(teams)
+    tournament.simulate_tournament()
+    tournament.ranking()
+                
+
+
+if __name__ == "__main__":
+    
+    run_simulation()
+
+
+
+
